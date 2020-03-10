c306c800239581143c85cda092146f1a1f479438
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-03-10 15:28:32 -0400

    Added a bigTitle event to page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+// this code here changes the Fun Bus title to a larger size when you hover over it
+let bigTitle = document.querySelector("header h1");
+
+bigTitle.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '7rem';
+})
+
+bigTitle.addEventListener('mouseout', function(event) {
+    event.target.style.fontSize = '4rem';
+})
+
+// Big Title ends here
+
