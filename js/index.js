417bdaf623511c064a287f33be0309ee96ec468b
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-03-10 16:46:18 -0400

    Added vroom event and introBackground event

--- js/index.js
+++ js/index.js
@@ -13,3 +13,16 @@ bigTitle.addEventListener('mouseout', function(event) {
 
 // Big Title ends here
 
+//vroom will change the 'Let's Go!' text to 'VROOM!'
+let vroom = document.querySelector(".text-content h2");
+
+vroom.addEventListener('click', function() {
+    vroom.textContent = 'VROOM!';
+})
+//vroom closes here
+
+let introBackground = document.querySelector('.intro h2');
+
+introBackground.addEventListener('dblclick', function(event) {
+    event.target.style.backgroundImage = 'linear-gradient(-90deg, red, yellow)';
+})
