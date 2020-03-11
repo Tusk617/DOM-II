74e22711f3c3db1da2eedfe77207782f4fdb2e97
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-03-11 11:10:40 -0400

    Added 10 unique events

--- js/index.js
+++ js/index.js
@@ -21,8 +21,56 @@ vroom.addEventListener('click', function() {
 })
 //vroom closes here
 
+//introBackground will change the background of the intro header to a gradient
 let introBackground = document.querySelector('.intro h2');
 
 introBackground.addEventListener('dblclick', function(event) {
     event.target.style.backgroundImage = 'linear-gradient(-90deg, red, yellow)';
+})
+//introBackground closes here
+
+// funNav will change the colors of each nav link by selecting it, and pressing R, B, G, and O
+let funNav = document.querySelector('nav');
+
+funNav.addEventListener('keydown', function(e) {
+    if (e.keyCode == '82') {
+        e.target.style.backgroundColor = 'red';
+    } else if (e.keyCode == '66') {
+        e.target.style.backgroundColor = 'blue';
+    } else if (e.keyCode == '71') {
+        e.target.style.backgroundColor = 'green';
+    } else if (e.keyCode == '79') {
+        e.target.style.backgroundColor = 'orange';
+    }
+})
+// funNav closes here
+
+var advImg = document.querySelector('.img-content img');
+
+advImg.addEventListener('mouseenter', function(event) {
+    event.target.style.width = '10%';
+})
+
+advImg.addEventListener('mouseleave', function(event) {
+    event.target.style.width = '100%'
+})
+
+let downMoused = document.querySelector('.text-content p');
+
+downMoused.addEventListener('mousedown', function(event) {
+    event.target.style.backgroundColor = 'red';
+})
+
+let movingMouse = document.querySelector('.content-destination h2');
+
+movingMouse.addEventListener('mousemove', function(event) {
+    event.target.textContent = 'Final Destination lol'
+})
+
+let overMouse = document.querySelector('.destination h4');
+
+overMouse.addEventListener('mouseover', function(event){
+    if (event.target.textContent === 'Fun In The Sun'){
+        event.target.textContent = 'Suns out, guns out';
+    }
 })
